a6fd4ed9dc34dd0dfdd08f535fbd1aaeb9a1868a
Eamon Linskey <Eamon.Linskey@gmail.com>
2018-04-08 21:44:35 +0000

    Edited Mailer files

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: "Eamon.Linskey@gmail.com" 
+   default to: 'Eamon.Linskey@gmail.com'
    
    def contact_email(name, email, body)
        @name = name
