6093fac3f2d5ded9744b3d321da0a53f48545e74
Eamon Linskey <Eamon.Linskey@gmail.com>
2018-04-11 20:46:19 +0000

    Added user profiles form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411165250) do
+ActiveRecord::Schema.define(version: 20180411204228) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180411165250) do
     t.text     "description"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "phone_number"
   end
 
   create_table "users", force: :cascade do |t|
